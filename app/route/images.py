8171e5c7308cda7d0e30d2c376576aca51643c60
nasri adzlani <nasriblog12@gmail.com>
2022-07-02 19:07:23 +0700

    add

--- app/route/images.py
+++ app/route/images.py
@@ -2,6 +2,8 @@
 import traceback
 from fastapi import APIRouter, UploadFile, File
 from fastapi.exceptions import HTTPException
+from fastapi.responses import FileResponse
+from utils import response
 import aiofiles
 app = APIRouter()
 
@@ -11,10 +13,17 @@ async def upload(image: UploadFile = File(...)):
     """ a function for insert images """
     try:
         destination_file_path = 'app/assets/'+image.filename
-        print(destination_file_path)
         async with aiofiles.open(destination_file_path, 'wb') as out_file:
             while content := await image.read(1024):
                 await out_file.write(content)
+        return response.response_detail(200, "Image success uploaded")
     except Exception as error:
         traceback.print_exc()
         raise HTTPException(400, "Failed Insert Images") from error
+
+@app.get('/load_image')
+async def load_image(image_name:str):
+    try:
+        return FileResponse('app/assets/'+image_name)
+    except Exception as error:
+        raise HTTPException(400, "Error load image") from error
