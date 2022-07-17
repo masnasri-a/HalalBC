63e0abdffec791758529f9d0b30cffb9ed26f4a4
masnasri-a <nasriblog12@gmail.com>
2022-07-17 15:46:48 +0700

    Feat:FixingSimulasi

--- app/logic/simulasi.py
+++ app/logic/simulasi.py
@@ -6,7 +6,6 @@ class Simulasi():
     def __init__(self,creator_id, registered) -> None:
         self.creator_id = creator_id
         self.registered = registered
-        print("simulasi")
 
     def get_list_bahan(self):
         client, col = mongo.mongodb_config('BahanDetail')
@@ -24,9 +23,28 @@ class Simulasi():
         list_bahan = self.get_list_bahan()
         if not self.registered:
             for detail_bahan in list_bahan:
-                
-            self.checking()
-
+                if detail_bahan['halal'] is not True:
+                    return {
+                        "message":"Sistem belum terintegrasi halal, Bahan masih ada yang belum bersertifasi halal",
+                        "data":None
+                    }, False
+                else :
+                    return {
+                        "message":"Sistem belum terintegrasi halal, Silahkan daftarkan produk anda",
+                        "data":None
+                    }, True
+        else:
+            for detail_bahan in list_bahan:
+                if detail_bahan['halal'] is not True:
+                    return {
+                        "message":"Sistem sudah terintegrasi halal, Bahan masih ada yang belum bersertifasi halal",
+                        "data":"Bahan masih ada yang belum bersertifasi halal"
+                    }, False
+                else :
+                    return {
+                        "message":"Sistem sudah terintegrasi halal, Silahkan daftarkan ulang produk anda",
+                        "data":"Silahkan daftarkan ulang produk anda"
+                    }, True
 
     def result(self):
-        self.get_list_bahan()
+        self.logic()
